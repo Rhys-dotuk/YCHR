e3928736f7d2d8b4a1890b75d6cb85930879e221
Rhys-dotuk <35027170+Rhys-dotuk@users.noreply.github.com>
2018-01-03 10:25:42 +0000

    Changes to index.php autoload and boostrap 2

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ echo phpversion ();
 
 ini_set('eaccelerator.enable', 0);
 
-require __DIR__.'/../vender/autoload.php';
+require __DIR__.'/vender/autoload.php';
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/bootstrap/app.php';
