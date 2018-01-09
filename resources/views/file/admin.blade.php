4ca09df9d2f1f6e906413f5bfe09b53c4351dd32
Rhys-dotuk <35027170+Rhys-dotuk@users.noreply.github.com>
2018-01-09 13:14:16 +0000

    Change to all file tables in doc area - changed to dataTables

--- resources/views/file/admin.blade.php
+++ resources/views/file/admin.blade.php
@@ -12,23 +12,19 @@
 	<div class="col-xs-6">
 		<div class="box-header">
 			<div class="box-tools">
-				<div class="input-group input-group-sm" style="width: 150px; margin: 20px; left: 20px;">
-					<input class="form-control" type="text" id="fileSearch" onkeyup="fileSearch()" placeholder="Search...">
-					<span class="input-group-btn" style="bottom: 1px;">
-						<button class="btn btn-indo btn-flat btn-primary glyphicon glyphicon-search" onkeyup="fileSearch()"></button>
-					</span>
-				</div>
 			</div>
 		</div>
 		<div class="box-body table-responsive no-padding">
-			<table id="tableRecords" class="table table-hover table-bordered table-striped" cellspacing="0"> 
-				<tbody class="table table-hover">
+			<table id="tableRecords" class="table table-hover table-bordered table-striped" cellspacing="0">
+				<thead class="table table-hover">
 					<tr>
 						<th>File Name</th>
 						<th>Created by</th>
 						<th></th>
 						<th></th>
 					</tr>
+				</thead> 
+				<tbody class="table table-hover">
 					@foreach($files as $file)
 						@if( Auth::user()->account_type != "U" && $file->company_name == Auth::user()->company_name && $file->folder_name == "admin")
 							@if( $file->file_status == "disabled" )
@@ -67,31 +63,18 @@
 	</div>
 </div>
 @stop
-<script>
-function fileSearch() 
-{
-	var searchText = document.getElementById('fileSearch').value; // Gets text entered into search bar
-	var targetTable = document.getElementById('tableRecords'); // Gets table data
-	var targetTableColCount;
-            
-	for (var rowIndex = 0; rowIndex < targetTable.rows.length; rowIndex++) { // Loops through each table row
-		var rowData = '';
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 
-		if (rowIndex == 0) {
-			targetTableColCount = targetTable.rows.item(rowIndex).cells.length;
-			continue;
-		}
-                
-		for (var colIndex = 0; colIndex < targetTableColCount; colIndex++) {
-			rowData += targetTable.rows.item(rowIndex).cells.item(colIndex).textContent; // Loops through table column
-		}
+<script type="text/javascript">
 
-		if (rowData.indexOf(searchText) == -1) // If data equals search -
-		{
-			targetTable.rows.item(rowIndex).style.display = 'none'; // - display row
-		} else {
-			targetTable.rows.item(rowIndex).style.display = 'table-row'; // - hide row
-		}
-	}
-}
+	$(document).ready(function() {
+		$('#tableRecords').DataTable({
+			bFilter: true,
+			bInfo: true,
+			bPaginate: true,
+			"iDisplayLength": 6,
+			"deferRender": true,
+		});
+	});
+
 </script>
