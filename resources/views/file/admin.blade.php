af32ff1b1d021685cb7e598a8a3aea78cb33c69e
Rhys-dotuk <35027170+Rhys-dotuk@users.noreply.github.com>
2018-01-09 14:10:28 +0000

    Test for admin doc area bug fix

--- resources/views/file/admin.blade.php
+++ resources/views/file/admin.blade.php
@@ -26,25 +26,18 @@
 				</thead> 
 				<tbody class="table table-hover">
 					@foreach($files as $file)
-						@if( Auth::user()->account_type != "U" && $file->company_name == Auth::user()->company_name && $file->folder_name == "admin")
-							@if( $file->file_status == "disabled" )
-
-							@else
-								<tr>
-									<td>{{ $file->file_name }}</td>
-									<td>{{ $file->name }}</td>
-									<td>
-										<form action="{{ route('mail.send', [ 'file_name' => $file->file_name, 'id' => Auth::user()->id ]) }}" method="GET">
-											{!! csrf_field() !!}
-											<input type="hidden" name="file_name" value="$file->file_name">
-											<button type="submit" class="btn btn-primary btn-block btn-flat">Download</button>
-										</form>
-									</td>
-								</tr>
-							@endif
-						@else
-
-						@endif
+						
+							<tr>
+								<td>{{ $file->file_name }}</td>
+								<td>{{ $file->name }}</td>
+								<td>
+									<form action="{{ route('mail.send', [ 'file_name' => $file->file_name, 'id' => Auth::user()->id ]) }}" method="GET">
+										{!! csrf_field() !!}
+										<input type="hidden" name="file_name" value="$file->file_name">
+										<button type="submit" class="btn btn-primary btn-block btn-flat">Download</button>
+									</form>
+								</td>
+							</tr>
 					@endforeach
 				</tbody>
 			</table>
